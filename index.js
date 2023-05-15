c0e623782c3d1b8d2f6e08f5b52164e8e30dfbf3
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-05-14 21:27:47 -0400

    Se añadieron los estilos, la imagen y el loading en la funcionalidad de JS

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ const setWeatherData =data=>{
         document.getElementById(key).textContent = weatherData[key];
     });
 
+    cleanUp();
+}
+
+const cleanUp= ()=>{
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex';
 }
 
 const getDate =()=> {
